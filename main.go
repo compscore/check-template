d5dde222e29eaaee695d405f3dc418a07b1b5c03
1nv8rZim <msf9542@rit.edu>
2023-08-31 21:35:59 -0400

    removed version variable from package

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"context"
-	_ "embed"
 )
-
-//go:embed version
-var Version string
 
 func Run(ctx context.Context, target string, command string, expectedOutput string, username string, password string) (bool, string) {
 	return true, "Successfully completed"
