cd169da6b4175dbe6ba4557e7e138969a8e2886e
1nv8rZim <msf9542@rit.edu>
2023-08-31 21:29:05 -0400

    simplified check function parameters

--- main.go
+++ main.go
@@ -8,12 +8,6 @@ import (
 //go:embed version
 var Version string
 
-func Run(ctx context.Context, checkStruct struct {
-	Target         string
-	Command        string
-	ExpectedOutput string
-	Username       string
-	Password       string
-}) (bool, string) {
+func Run(ctx context.Context, target string, command string, ExpectedOutput string, username string, password string) (bool, string) {
 	return true, "Successfully completed"
 }
